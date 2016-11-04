fdd4dfabb07a39547b4fdcf1495922fd009b51f6
Jordan Ross <jordanross909@gmail.com>
2016-11-04 18:11:02 -0400

    Added input, memory management

--- core/src/com/example/reflectbeat/HitCircle.java
+++ core/src/com/example/reflectbeat/HitCircle.java
@@ -2,6 +2,7 @@ package com.example.reflectbeat;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Pool;
 
 import java.util.Locale;
 
@@ -9,17 +10,19 @@ import java.util.Locale;
  * Created by Jordan on 11/3/2016.
  */
 
-public class HitCircle extends Sprite {
+public class HitCircle extends Sprite implements Pool.Poolable {
 
+    private float xspeed, yspeed;
     private int x_direction; // -1 is left
+    public boolean alive;
 
-    public HitCircle(boolean fail, float x, float y, int xdir) {
+    public HitCircle(boolean fail, float x, float y, int xdir, float xspeed, float yspeed) {
         //super(fail ? "hitcircle_fail.png" : "hitcircle.png");
         super(fail ? ReflectBeat.hitcircle_fail_texture : ReflectBeat.hitcircle_texture);
 
         this.setPosition(x, y);
-        // TODO: Make this not suck
 
+        // TODO: Make this not suck
         if (this.getX() < 2)
             x_direction = 1;
         else if (this.getX() > ReflectBeat.RENDER_WIDTH - ReflectBeat.HIT_SPRITE_SIZE - 2)
@@ -27,12 +30,22 @@ public class HitCircle extends Sprite {
         else
             x_direction = xdir;
 
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+
+        this.alive = false;
 
         Gdx.app.log("HitCircle constructor", String.format(Locale.US, "x: %f   y: %f    xdir: %d",
                 getX(), getY(), x_direction));
     }
 
-    public void move(float xAmount, float yAmount) {
+    public HitCircle(boolean fail, float x, float y, int xdir) {
+        this(fail, x, y, xdir, 0, -300);
+    }
+
+    public void moveCircle(float deltaTime) {
+        float xAmount = xspeed * deltaTime;
+        float yAmount = yspeed * deltaTime;
         if (getX() + xAmount > ReflectBeat.RENDER_WIDTH - ReflectBeat.HIT_SPRITE_SIZE
                 || getX() - xAmount < 0) {
             x_direction = -1 * x_direction;
@@ -40,5 +53,19 @@ public class HitCircle extends Sprite {
         // Warning: don't use this it's dumb
         //setRotation(getRotation() + x_direction * -4);
         super.translate((float) x_direction * xAmount, yAmount);
+    }
+
+    public void init(boolean fail, float xspeed, float yspeed) {
+        this.setTexture(fail ? ReflectBeat.hitcircle_fail_texture : ReflectBeat.hitcircle_texture);
+        setPosition(ReflectBeat.RENDER_WIDTH/2, ReflectBeat.RENDER_HEIGHT);
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+        alive = true;
+    }
+
+    @Override
+    public void reset() {
+        setPosition(0,0);
+        alive = false;
     }
 }
--- core/src/com/example/reflectbeat/ReflectBeat.java
+++ core/src/com/example/reflectbeat/ReflectBeat.java
@@ -3,12 +3,17 @@ package com.example.reflectbeat;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.utils.StringBuilder;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -19,9 +24,11 @@ public class ReflectBeat extends ApplicationAdapter {
     public Viewport viewport;
     private OrthographicCamera camera;
     private SpriteBatch batch;
-    private Texture hit_line;
-    private Array<HitCircle> hitcircles;
+    private Sprite hit_line;
+    private Pool<HitCircle> hitCirclePool;
+    private Array<HitCircle> active_hitcircles;
     private Random random;
+    BitmapFont font;
 
     public static final int RENDER_WIDTH = 540;
     public static final int RENDER_HEIGHT = 960;
@@ -30,8 +37,10 @@ public class ReflectBeat extends ApplicationAdapter {
 
     public static Texture hitcircle_texture;
     public static Texture hitcircle_fail_texture;
-    //Test
+    private Texture hit_line_texture;
+
     private float speed = 300;
+    private int score;
 
     @Override
     public void create() {
@@ -52,23 +61,125 @@ public class ReflectBeat extends ApplicationAdapter {
             @Override
             public boolean touchDown (int x, int y, int pointer, int button) {
                 //Gdx.app.log("touchDown", String.format(Locale.US, "Touch location: %d, %d", x, y));
-                Vector2 points = new Vector2(x, y);
-                points = viewport.unproject(points);
+                Vector2 transform = new Vector2(x, y);
+                transform = viewport.unproject(transform);
                 Gdx.app.log("touchDown", String.format(Locale.US,
-                        "Touch location (Transformed): %f, %f", points.x, points.y));
+                        "Touch location (Transformed): %f, %f", transform.x, transform.y));
+
+                //TODO: Improve this process
+                //This is going through *every* hitcircle on the screen to check for touch,
+                //which is probably going to suck later (like when there are 100 circles to check)
+                int size = active_hitcircles.size;
+                for (int i = 0; i < size; i++ ) {
+                    // TODO: Calibrate hitboxes
+                    if (hit_line.getBoundingRectangle().contains(transform.x, transform.y)) {
+                        //Tapped inside hitline, so check for hitCirclePool
+                        if (active_hitcircles.get(i).getBoundingRectangle().contains(transform.x, transform.y)) {
+                            // Hitcircle was touched while one line
+                            Gdx.app.log("touchDown", "Hitcircle touched!");
+                            HitCircle hit = active_hitcircles.get(i);
+                            // Remove hit
+                            hit.alive = false;
+                            spawnHitcircle(0, -speed);
+
+                            score++;
+                        }
+                    }
+                }
 
                 return true; // return true to indicate the event was handled
             }
         });
 
         random = new Random();
+        font = new BitmapFont();
+        font.getData().setScale(20, 20);
 
         hitcircle_texture = new Texture("hitcircle.png");
         hitcircle_fail_texture = new Texture("hitcircle_fail.png");
-        hit_line = new Texture("hitline.png");
+        hit_line_texture = new Texture("hitline.png");
 
-        hitcircles = new Array<HitCircle>();
-        spawnHitcircle();
+        hit_line = new Sprite(hit_line_texture);
+        hit_line.setPosition(0, LINE_HEIGHT);
+
+        hitCirclePool = new Pool<HitCircle>() {
+            @Override
+            protected HitCircle newObject() {
+                return new  HitCircle(
+                        false,
+                        (float) random.nextInt(RENDER_WIDTH - 2 * HIT_SPRITE_SIZE) + HIT_SPRITE_SIZE,
+                        RENDER_HEIGHT,
+                        random.nextBoolean() ? 1 : -1
+                );
+            }
+        };
+
+        active_hitcircles = new Array<HitCircle>();
+
+        spawnHitcircle(0, -speed);
+
+        startSong();
+
+        score = 0;
+    }
+
+    private void startSong() {
+        // This is a stream, i.e. not loaded in ram.
+        Music music = Gdx.audio.newMusic(Gdx.files.internal("clicktrack.mp3"));
+        music.setLooping(true);
+        music.play();
+
+
+
+    }
+
+    // TODO: Add x/y pos
+    private void spawnHitcircle(float xspeed, float yspeed) {
+        //hitCirclePool.add(new HitCircle(
+        //        false,
+        //        (float) random.nextInt(RENDER_WIDTH - 2 * HIT_SPRITE_SIZE) + HIT_SPRITE_SIZE,
+        //        RENDER_HEIGHT,
+        //        random.nextBoolean() ? 1 : -1,
+        //        xspeed,
+        //        yspeed
+        //));
+        HitCircle hit = hitCirclePool.obtain();
+        hit.init(false, xspeed, yspeed);
+        active_hitcircles.add(hit);
+    }
+
+    private void removeHitcircles() {
+        for (int i = 0; i < active_hitcircles.size; i++) {
+            HitCircle hit = active_hitcircles.get(i);
+            if (!hit.alive) {
+                active_hitcircles.removeIndex(i);
+                hitCirclePool.free(hit);
+            }
+        }
+    }
+
+
+    //Handle Hitcircle movement
+    private void moveHitcircles() {
+        int size = active_hitcircles.size;
+        for (int i = 0; i < size; i++) {
+            HitCircle hit = active_hitcircles.get(i);
+
+            hit.moveCircle(Gdx.graphics.getDeltaTime());
+
+            // If hitcircle passed below line
+            if (hit.getY() < LINE_HEIGHT - HIT_SPRITE_SIZE) {
+                if (hit.getY() < -HIT_SPRITE_SIZE) {
+                    // Below Screen (Remove hitcircle)
+                    hit.alive = false;
+                    spawnHitcircle(0, -speed);
+                }
+                else {
+                    // Just below line (Hit fail)
+                    hit.setTexture(hitcircle_fail_texture);
+                }
+            }
+        }
     }
 
     @Override
@@ -81,56 +192,36 @@ public class ReflectBeat extends ApplicationAdapter {
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
+        // TODO: Custom score font? Actually, just fix the score thing in general
+        font.draw(batch, Integer.toString(score), camera.viewportWidth/2 - 70, camera.viewportHeight/2);
         batch.draw(hit_line, 0, LINE_HEIGHT);
-        for (int i = 0; i < hitcircles.size; i++ ) {
-            HitCircle hit = hitcircles.get(i);
+        for (int i = 0; i < active_hitcircles.size; i++ ) {
+            HitCircle hit = active_hitcircles.get(i);
             hit.draw(batch);
+
         }
+
         batch.end();
+
+        removeHitcircles();
     }
 
-    private void spawnHitcircle() {
-        hitcircles.add(new HitCircle(
-                false,
-                (float) random.nextInt(RENDER_WIDTH - 2 * HIT_SPRITE_SIZE) + HIT_SPRITE_SIZE,
-                RENDER_HEIGHT,
-                random.nextBoolean() ? 1 : -1)
-        );
-    }
 
-    //Handle Hitcircle movement
-    private void moveHitcircles() {
-        for (int i = 0; i < hitcircles.size; i++) {
-            HitCircle hit = hitcircles.get(i);
 
-            hit.move(speed * Gdx.graphics.getDeltaTime(), -speed * Gdx.graphics.getDeltaTime());
-
-            // If hitcircle passed below line
-            if (hit.getY() < LINE_HEIGHT - HIT_SPRITE_SIZE) {
-                if (hit.getY() < -HIT_SPRITE_SIZE) {
-                    // Below Screen (Remove hitcircle)
-                    hitcircles.removeIndex(i);
-                    spawnHitcircle();
-                }
-                else {
-                    // Just below line (Hit fail)
-                    hit.setTexture(hitcircle_fail_texture);
-                }
-            }
-        }
-    }
-
+    @Override
     public void resize(int width, int height){
         //super.resize(width, height);
         //batch.getProjectionMatrix().setToOrtho2D(0, 0, RENDER_WIDTH, RENDER_HEIGHT);
         viewport.update(width,height);
         camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
     }
+
     @Override
     public void dispose() {
         batch.dispose();
         hitcircle_fail_texture.dispose();
         hitcircle_texture.dispose();
-        hit_line.dispose();
+        hit_line_texture.dispose();
+        hitCirclePool.freeAll(active_hitcircles);
     }
 }
