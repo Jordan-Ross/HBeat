ff782aefa7098974def5e247bd234e6ff71be267
Jordan Ross <jordanross909@gmail.com>
2016-12-09 14:50:30 -0500

    Simplified script

--- scripts/EditMap.py
+++ scripts/EditMap.py
@@ -1,33 +1,13 @@
 print("Filename (no ext or dir):")
 filename = raw_input()
-inf = open("E:\\Documents\\Android Projects\\reflectbeat\\android\\assets\\songs\\" + filename + ".map", 'r')
-outf = open("E:\\Documents\\Android Projects\\reflectbeat\\android\\assets\\songs\\" + filename + ".rbm", 'w')
-
-
-
-
-# Because the original maps have the time the circles _appear_, 
-# we need to calculate the time_to_get_to_line, because the 
-# circles will actually spawn this amount of time before.
-
-# Calculations will be done here for now
-
-#time_to_get_to_line = distance_between_spawn_and_hit_spot / y_speed
-#map_height = 960
-##line height - 100, line width/2 = 20
-#hit_spot_height = 120
-#y_speed = 300
-#time_to_get_to_line = float(map_height - hit_spot_height)/float(y_speed)
-
-
-
+path = "E:\\Documents\\Android Projects\\reflectbeat\\android\\assets\\songs\\"
+inf = open(path + filename + ".map", 'r')
+outf = open(path + filename + ".rbm", 'w')
 
 for line in inf:
 	splitln = line.split(',')
-	
-	#Adjust timing
-	time = int(splitln[2])
-	#time = time - time_to_get_to_line
-	outf.write(str(time) + ',' + splitln[0] + ',' + splitln[1] + '\n')
+	#time = int(splitln[2])
+	#outf.write(str(time) + ',' + splitln[0] + ',' + splitln[1] + '\n')
+	outf.write(splitln[2] + ',' + splitln[0] + ',' + splitln[1] + '\n')
 
 outf.close()
